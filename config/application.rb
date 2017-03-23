5b62db70666e95d9b187f7133639fb785bcc492e
afkza4aem <nastypinagrably@gmail.com>
2017-03-23 19:16:03 +0300

    add postmark

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module ToyApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_mailer.delivery_method = :postmark
+config.action_mailer.postmark_settings = { :api_token => "91e85b9e-402b-4948-8ee0-d166dadd4aff" }
   end
 end
